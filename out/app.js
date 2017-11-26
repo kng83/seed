c346c135744dc27bcbe1a404112abb89cc1f1f5d
kng83 <pawel.kng@gmail.com>
2017-11-26 21:00:59 +0100

    first commit

--- out/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var _ = require("lodash");
-var bobo = function () { console.log('Hi there'); };
-bobo();
-var myArr = [1, 2, 3, 5, 6, 7];
-var check = _.floor(4.55);
-console.log(check);
-//# sourceMappingURL=app.js.map
